dd4c8264b3e8b7bd7df278ac1ef8f803bbb027c6
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2024-10-17 14:24:07 +0300

    Add Task3

---
+++ Задание3/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputTextBox.Text;
+            if (string.IsNullOrEmpty(input) || !IsNumeric(input))
+            {
+                resultTextBlock.Text = "Ошибка: Введите только цифры.";
+            }
+            else
+            {
+                resultTextBlock.Text = "Все хорошо!";
+            }
+        }
+
+        private bool IsNumeric(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Задание3/WpfApp1/Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp;
+using WpfApp1;
+
+namespace WpfApp.Tests
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+        [TestMethod]
+        public void TestIsNumeric_WithOnlyDigits_ReturnsTrue()
+        {
+            var window = new MainWindow();
+            Assert.IsTrue(window.IsNumeric("12345"));
+        }
+
+        [TestMethod]
+        public void TestIsNumeric_WithLetters_ReturnsFalse()
+        {
+            var window = new MainWindow();
+            Assert.IsFalse(window.IsNumeric("123a45"));
+        }
+
+        [TestMethod]
+        public void TestIsNumeric_WithEmptyString_ReturnsFalse()
+        {
+            var window = new MainWindow();
+            Assert.IsFalse(window.IsNumeric(""));
+        }
+
+        [TestMethod]
+        public void TestIsNumeric_WithSpecialCharacters_ReturnsFalse()
+        {
+            var window = new MainWindow();
+            Assert.IsFalse(window.IsNumeric("123@45"));
+        }
+    }
+}
